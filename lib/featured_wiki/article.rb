5bbd2a79def4c885c67626fb533914af3b59f6d2
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-09-28 09:25:26 -0400

    add iterator to #initialize

--- lib/featured_wiki/article.rb
+++ lib/featured_wiki/article.rb
@@ -1,10 +1,15 @@
 class FeaturedWiki::Article
   attr_accessor :title, :summary, :url
 
-    def initialize
-      @title = "Spotted Green Pigeon"
-      @summary = "The spotted green pigeon is a species of pigeon that is most likely extinct. It was first mentioned and described in 1783 by John Latham, who claimed to have seen two specimens and a drawing depicting the bird. Today, the species is only known from a specimen kept in World Museum, Liverpool. Overlooked for much of the 20th century, it was only recognised as a valid extinct species by the IUCN Red List in 2008. In 2014 a genetic study confirmed it as a distinct species related to the Nicobar pigeon, and showed that the two were the closest relatives of the extinct dodo and Rodrigues solitaire. The remaining specimen is 32 cm (12.5 in) long, and has very dark, brownish plumage with a green gloss. The neck-feathers are elongated, and most of the feathers on the upperparts and wings have a yellowish spot on their tips. It has a black bill with a yellow tip, and the end of the tail has a pale band. It has relatively short legs and long wings. It may have been native to an island somewhere in the South Pacific Ocean or the Indian Ocean, and it has been suggested that a bird referred to in 1928 as titi by Tahitian islanders was this bird."
-      @url = "https://en.wikipedia.org/wiki/Spotted_green_pigeon"
+  @@all = []
+
+    def initialize(article_hash)
+      article_hash.each { |key, value| self.send("#{key}=", value) }
+      @@all << self
+    end
+
+    def self.all
+      @@all
     end
 
 end
