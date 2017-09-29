890729eb21668759dd3b47daedc7f800d761b4d2
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-09-29 16:53:33 -0400

    change formatting in #menu

--- lib/featured_wiki/cli.rb
+++ lib/featured_wiki/cli.rb
@@ -5,24 +5,31 @@ class FeaturedWiki::CLI
   end
 
   def menu
-    puts "\nWelcome to Today's Featured Wiki!\n\n"
+    puts "\nWelcome to Today's Featured Wikipedia Article!\n\n"
     puts "Enter the number you'd like to check out:\n\n"
-    puts "1. See today's featured wiki article."
-    puts "2. View this month's featured article queue."
-    puts "3. Get info on Wikipedia's Featured Article Section."
+    puts "1. See today's featured article."
+    puts "2. See the most viewed Today's Featured Wikis."
+    puts "3. Get more info on Wikipedia's Today's Featured Article Section."
 
     puts "\nWhat would you like to do?"
     input = nil
     while input != "exit"
       input = gets.strip.downcase
         if input == "1"
-          puts "#{todays_article.title}"
+          puts ""
+          puts "Title: #{todays_article.title}"
+          puts ""
+          puts "Blurb:"
+          puts ""
           puts "#{todays_article.blurb}"
-          puts "Read the full article at: #{todays_article.url}"
+          puts ""
+          puts "Read the full article here: #{todays_article.url}"
+          puts ""
         elsif input == "2"
           puts "Show most viewed featured articles..."
           most_viewed_articles
           FeaturedWiki::Article.all.each.with_index(1) do |a, i|
+            puts ""
             puts "#{i}. #{a.title}"
             puts ""
             puts "Featured date: #{a.featured_date}"
