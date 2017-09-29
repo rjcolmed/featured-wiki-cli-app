8c7e9456cd5f4f2478c75b781060ed52c2212469
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-09-29 11:35:30 -0400

    move scraping mthods into new Scraper class

--- lib/featured_wiki/article.rb
+++ lib/featured_wiki/article.rb
@@ -1,8 +1,7 @@
 class FeaturedWiki::Article
-  attr_accessor :todays_title, :todays_summary, :todays_url, :recently_featured_titles, :recently_featured_urls
+  attr_accessor :todays_title, :todays_blurb, :todays_url
 
   @@all = []
-  BASE_URL = "https://en.wikipedia.org"
 
     def initialize(article_hash)
       article_hash.each { |key, value| self.send("#{key}=", value) }
@@ -12,33 +11,4 @@ class FeaturedWiki::Article
     def self.all
       @@all
     end
-
-    def self.today
-      # scrape wikipedia and return title, summary, and url of Today's featured article
-      self.scrape_main_page(BASE_URL)
-    end
-
-    def self.recently_featured
-      self.scrape_main_page(BASE_URL)
-    end
-
-    def self.scrape_main_page(main_page_url)
-      doc = Nokogiri::HTML(open(main_page_url))
-
-      titles = []
-      urls = []
-      doc.css("div.tfa-recent a").each do |link|
-        titles << link.text
-        urls << BASE_URL + link["href"]
-      end
-
-      data = {
-        :todays_title => doc.css("div#mp-tfa b a").shift.attribute("title").text,
-        :todays_summary => doc.css("div#mp-tfa").text.split(" (Full").shift,
-        :todays_url => BASE_URL + doc.css("div#mp-tfa a.mw-redirect").attribute("href").text,
-        :recently_featured_titles => titles,
-        :recently_featured_urls => urls
-      }
-    end
-
 end
