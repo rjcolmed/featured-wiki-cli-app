b12d81d2866e4f180a0c68ce51db1dfd604be304
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-09-29 16:26:54 -0400

    remove simple tests from #menu, blank #display, and 'Choose from a few recently featured articles' menu item and related conditional branch from #menu. add #todays_article and #most_viewed_articles and simple puts displays to #menu for two of its conditional branches

--- lib/featured_wiki/cli.rb
+++ lib/featured_wiki/cli.rb
@@ -5,32 +5,37 @@ class FeaturedWiki::CLI
   end
 
   def menu
-    puts "\nWelcome to Featured Wiki!\n\n"
+    puts "\nWelcome to Today's Featured Wiki!\n\n"
     puts "Enter the number you'd like to check out:\n\n"
     puts "1. See today's featured wiki article."
-    puts "2. Choose from a few recently featured articles."
-    puts "3. View this month's featured article queue."
-    puts "4. Get info on Wikipedia's Featured Article Section."
+    puts "2. View this month's featured article queue."
+    puts "3. Get info on Wikipedia's Featured Article Section."
 
     puts "\nWhat would you like to do?"
     input = nil
     while input != "exit"
       input = gets.strip.downcase
         if input == "1"
-          puts "Showing today's feature article.."
-          todays_article = FeaturedWiki::Scraper.scrape_featured_article_page
-          puts "#{todays_article[:title]}"
-          puts "#{todays_article[:blurb]}"
-          puts "Read the full article at: #{todays_article[:url]}"
+          puts "#{todays_article.title}"
+          puts "#{todays_article.blurb}"
+          puts "Read the full article at: #{todays_article.url}"
         elsif input == "2"
-          puts "Listing recently featured articles...\n"
-        elsif input == "3"
           puts "Show most viewed featured articles..."
-          FeaturedWiki::Article.create_articles_from_collection(FeaturedWiki::Scraper.scrape_most_viewed_page)
-          FeaturedWiki::Article.all.each.with_index(1) do |article, i|
-            puts "#{i}. #{article.title} - #{article.views}"
+          most_viewed_articles
+          FeaturedWiki::Article.all.each.with_index(1) do |a, i|
+            puts "#{i}. #{a.title}"
+            puts "#{a.featured_date}"
+            puts "#{a.views}"
+            puts ""
+            puts "Blurb:"
+            puts ""
+            puts "#{a.blurb}"
+            puts ""
+            puts "Read the full article here: #{a.url}"
+            puts "===================================="
+            puts ""
           end
-        elsif input == "4"
+        elsif input == "3"
           puts "Showing info on Wikipedia's Featured Article Section..."
         elsif input == "exit"
           puts "Bye!"
@@ -41,8 +46,15 @@ class FeaturedWiki::CLI
         end
         menu
     end
+  end
 
-    def display
-    end
+  def todays_article
+    todays_article = FeaturedWiki::Scraper.scrape_featured_article_page
+    FeaturedWiki::Article.new(todays_article)
+  end
+
+  def most_viewed_articles
+    most_viewed = FeaturedWiki::Scraper.scrape_most_viewed_page
+    FeaturedWiki::Article.create_most_viewed_articles(most_viewed)
   end
 end
