750392ee8d44549d31ab456a3483b79a02162e5f
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-09-29 16:46:35 -0400

    move save to @@all functionality from #initialize to #create_most_viewed_articles. refactor #create_most_viewed_articles

--- lib/featured_wiki/article.rb
+++ lib/featured_wiki/article.rb
@@ -5,7 +5,6 @@ class FeaturedWiki::Article
 
     def initialize(article_hash)
       article_hash.each { |key, value| self.send("#{key}=", value) }
-      @@all << self
     end
 
     def self.all
@@ -13,7 +12,6 @@ class FeaturedWiki::Article
     end
 
     def self.create_most_viewed_articles(collection)
-      collection.each { |article_hash| self.new(article_hash) }
+      collection.each { |article_hash| @@all << self.new(article_hash) }
     end
-
 end
