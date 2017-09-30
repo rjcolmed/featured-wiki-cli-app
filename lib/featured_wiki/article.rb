8ea89557b6e839252e903beb39f042af5d920e15
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-09-30 17:29:00 -0400

    remove @@all. add @@all_most_viewed and @@all_this_months. add .all_most_viewed and .all_this_months. add .create_this_months_articles

--- lib/featured_wiki/article.rb
+++ lib/featured_wiki/article.rb
@@ -1,17 +1,26 @@
 class FeaturedWiki::Article
   attr_accessor :title, :blurb, :url, :featured_date, :views
 
-  @@all = []
+  @@all_most_viewed = []
+  @@all_this_months = [] #added
 
-    def initialize(article_hash)
-      article_hash.each { |key, value| self.send("#{key}=", value) }
+    def initialize(hash)
+      hash.each { |key, value| self.send("#{key}=", value) }
     end
 
-    def self.all
-      @@all
+    def self.all_most_viewed
+      @@all_most_viewed
+    end
+
+    def self.all_this_months
+      @@all_this_months
     end
 
     def self.create_most_viewed_articles(collection)
-      collection.each { |article_hash| @@all << self.new(article_hash) }
+      collection.each { |hash| @@all_most_viewed << self.new(hash) }
+    end
+
+    def self.create_this_months_articles(collection) #added
+      collection.each { |hash| @@all_this_months << self.new(hash) }
     end
 end
