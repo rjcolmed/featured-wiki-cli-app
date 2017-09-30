3e395eeac6e415f4151ea1a6384ab50aa56943f2
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-09-30 18:10:07 -0400

    remove .create_articles_from_collection. add .create_and_save_most_viewed_from and .create_and_save_this_months_from

--- lib/featured_wiki/article.rb
+++ lib/featured_wiki/article.rb
@@ -2,7 +2,7 @@ class FeaturedWiki::Article
   attr_accessor :title, :blurb, :url, :featured_date, :views
 
   @@all_most_viewed = []
-  @@all_this_months = [] #added
+  @@all_this_months = []
 
     def initialize(hash)
       hash.each { |key, value| self.send("#{key}=", value) }
@@ -16,11 +16,11 @@ class FeaturedWiki::Article
       @@all_this_months
     end
 
-    def self.create_most_viewed_articles(collection)
+    def self.create_and_save_most_viewed_from(collection)
       collection.each { |hash| @@all_most_viewed << self.new(hash) }
     end
 
-    def self.create_this_months_articles(collection) #added
+    def self.create_and_save_this_months_from(collection)
       collection.each { |hash| @@all_this_months << self.new(hash) }
     end
 end
