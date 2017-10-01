24a454a65706800d1a98310a1f8c5ae92f86e718
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-01 17:12:11 -0400

    refactor .scrape_this_months_page and .scrape_most_viewed_page. remove MOST_VIEWED class constant

--- lib/featured_wiki/scraper.rb
+++ lib/featured_wiki/scraper.rb
@@ -1,10 +1,9 @@
 class FeaturedWiki::Scraper
 
-  BASE_URL = "https://en.wikipedia.org/wiki/Wikipedia:Today%27s_featured_article"
   HOME_URL = "https://en.wikipedia.org"
-  DATE_PATH = "/#{Date.today.strftime("%B")}_#{Date.today.day},_#{Date.today.year}"
   MONTH_PATH = "/#{Date.today.strftime("%B")}_#{Date.today.year}"
-  MOST_VIEWED_PATH = "/Most_viewed"
+  BASE_URL = "https://en.wikipedia.org/wiki/Wikipedia:Today%27s_featured_article"
+  DATE_PATH = "/#{Date.today.strftime("%B")}_#{Date.today.day},_#{Date.today.year}"
 
   def self.scrape_featured_article_page
     doc = Nokogiri::HTML(open(BASE_URL + DATE_PATH))
@@ -19,35 +18,37 @@ class FeaturedWiki::Scraper
   def self.scrape_this_months_page
     doc = Nokogiri::HTML(open(BASE_URL + MONTH_PATH))
     doc.css("div#mp-tfa-img + p, dl + p").each_with_index.map do |p, i|
-        doc.css("dt b")[i] ? d = doc.css("dt b")[i].text : d = "No date found"
-        p.at_css('a:has(b)') ? t = p.css('a:has(b)').attribute('title').text : t = "No title found"
         p ? b = p.text.split(" (Full").first : b = "No blurb found"
-        p.at_css('a:has(b)') ? u = HOME_URL + p.css('a:has(b)').attribute('href').text : u = "No URL found"
+        doc.css("dt b")[i] ? d = doc.css("dt b")[i].text : d = "No date found"
+        p.at_css("a:has(b)") ? t = p.css("a:has(b)")[0]["title"] : t = "No title found"
+        p.at_css("a:has(b)") ? u = HOME_URL + p.css("a:has(b)")[0]["href"] : u = "No URL found"
 
         { featured_date: d, title: t, blurb: b, url: u }
     end
   end
 
   def self.scrape_most_viewed_page
-    doc = Nokogiri::HTML(open(BASE_URL + MOST_VIEWED_PATH))
+    doc = Nokogiri::HTML(open(BASE_URL + "/Most_viewed"))
+
     most_viewed = []
-    doc.css("table").each do |table|
-      table.css("tr").each do |row|
-        article = {}
-        row.css("td").each_with_index do |col, i|
-          if i == 0
-              article[:title] = col.css("a").text
-              article[:url] = HOME_URL + col.css("a").attribute("href").text
-              article[:featured_date] = col.css("small").text
-          elsif i == 2
-            article[:views] = col.css("a").text + " " + col.css("small").text
-          elsif i == 3
-            article[:blurb] = col.text
-          end
+    tables = doc.css("table")
+    tables.css("tr").each do |row|
+      article = {}
+      row.css("td").each_with_index do |col, i|
+        case i
+        when 0
+          article[:title] = col.css("a").text
+          article[:featured_date] = col.css("small").text
+          article[:url] = "#{HOME_URL}#{col.css("a")[0]["href"]}"
+        when 2
+          article[:views] = "#{col.css("a").text} #{col.css("small").text}"
+        when 3
+          article[:blurb] = col.text
         end
-        most_viewed << article
       end
+      most_viewed << article
     end
     most_viewed.drop(1)
   end
+
 end
