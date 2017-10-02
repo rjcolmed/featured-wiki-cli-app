56dd79d5647334c16a0129c35695606c1bc7344d
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-02 13:07:48 -0400

    remove attr_accessor :featured_date, @@all_most_viewed, .all_most_viewed, .find_most_viewed

--- lib/featured_wiki/article.rb
+++ lib/featured_wiki/article.rb
@@ -1,21 +1,7 @@
 class FeaturedWiki::Article
-  attr_accessor :title, :blurb, :url, :featured_date, :views
-
-  @@all_most_viewed = []
+  attr_accessor :title, :blurb, :url
 
     def initialize(hash)
       hash.each { |key, value| self.send("#{key}=", value) }
-    end
-
-    def self.all_most_viewed
-      @@all_most_viewed
-    end
-
-    def self.find_most_viewed(i)
-      @@all_most_viewed[i - 1]
-    end
-
-    def self.create_and_save_most_viewed_from(collection)
-      collection.each { |hash| @@all_most_viewed << self.new(hash) }
     end
 end
