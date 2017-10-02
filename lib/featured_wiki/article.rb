038aec85a6cefa8f2689bde0d804bae414158b68
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-02 14:13:30 -0400

    add @@all, .all, .find_article_by, create_and_save_from, and attr_accessor :featured_date

--- lib/featured_wiki/article.rb
+++ lib/featured_wiki/article.rb
@@ -1,7 +1,21 @@
 class FeaturedWiki::Article
-  attr_accessor :title, :blurb, :url
+  attr_accessor :title, :blurb, :url, :featured_date
+
+  @@all = []
 
     def initialize(hash)
       hash.each { |key, value| self.send("#{key}=", value) }
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.find_article_by(id)
+      @@all[id - 1]
+    end
+
+    def self.create_and_save_from(hashes)
+      hashes.each { |hash| @@all << self.new(hash) }
     end
 end
